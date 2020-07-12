840b28732125e3ff8032af8b18a3cf5045c1b669
Omar Malavé <omarmalave21@gmail.com>
2020-07-12 19:02:32 -0400

    password reset

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -26,11 +26,19 @@ urlpatterns = [
     path('sign_up', users_views.sign_up, name='sign_up'),
     path('sign_in', auth_views.LoginView.as_view(template_name='users/sign_in.html'), name='sign_in'),
     path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='log_out'),
+    path('password-reset', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
     path('profile', users_views.profile, name='profile'),
     path('', include('blog.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
